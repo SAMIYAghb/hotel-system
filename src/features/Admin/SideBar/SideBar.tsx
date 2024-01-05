acb0b5c5771714180c20bee0fdb81ebfbd783f86
Hadeer Magdy <dody.magdy711@gmail.com>
2024-01-05 13:59:12 +0200

    update sidebare

--- src/features/Admin/SideBar/SideBar.tsx
+++ src/features/Admin/SideBar/SideBar.tsx
@@ -1,115 +1,84 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../../../context/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
 import { Menu, MenuItem, Sidebar } from 'react-pro-sidebar';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
- import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
- import GridViewIcon from '@mui/icons-material/GridView';
- import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
- import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
- import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
- import LogoutIcon from '@mui/icons-material/Logout';
-
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import GridViewIcon from '@mui/icons-material/GridView';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const SideBar: React.FC = () => {
-  // let { userRole } = useContext(AuthContext);
-  
-
-  //*************sidebar collapse***************
   let [isCollapsed, setIsCollapsed] = useState(false);
   let handleToggle = () => {
     setIsCollapsed(!isCollapsed);
   };
-  //  logout Function
+
   const navigate = useNavigate();
   function logOut(): void {
     localStorage.removeItem("userToken");
     navigate("/login");
   }
 
-  //Model
-  // const [show, setShow] = useState(false);
-
-  // const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
-
   return (
-    <div className="sidebar-container">
-      {/* <Modal
-        show={show}
-        onHide={handleClose}
-        size="lg"
-        // className="custom-model"
-        dialogClassName="custom-model"
-      >
-        <Modal.Body className="p-0">
-          <ChangePassword handleClose={handleClose} />
-        </Modal.Body>
-      </Modal> */}
-      <Sidebar className="vh-100" collapsed={isCollapsed}>
+    <div className="sidebar-container" >
+      <Sidebar collapsed={isCollapsed}>
         <div
           className={`toggle-sidebar-btn ${isCollapsed ? "collapsed" : ""} `}
           onClick={handleToggle}
         >
           {isCollapsed ? (
-            <KeyboardDoubleArrowRightIcon/>
+            <KeyboardDoubleArrowRightIcon />
           ) : (
-            <KeyboardDoubleArrowLeftIcon/>
+            <KeyboardDoubleArrowLeftIcon />
           )}
         </div>
-       
-        <Menu className="mt-5">
-         
+
+        <Menu className="menu-space">
           <MenuItem
-            icon={<AccountBalanceIcon/>}
+            icon={<AccountBalanceIcon />}
             component={<Link to="/home" />}
           >
             Home
           </MenuItem>
-        <MenuItem
-            icon={<PeopleAltIcon/>}
+          <MenuItem
+            icon={<PeopleAltIcon />}
             component={<Link to="/home/users" />}
           >
             Users
           </MenuItem>
           <MenuItem
-            icon={<GridViewIcon/>}
+            icon={<GridViewIcon />}
             component={<Link to="/home/rooms" />}
           >
             Rooms
           </MenuItem>
-        <MenuItem
-            icon={<CardGiftcardIcon/>}
+          <MenuItem
+            icon={<CardGiftcardIcon />}
             component={<Link to="/home/ads" />}
           >
             Ads
           </MenuItem>
           <MenuItem
-            icon={<CalendarMonthIcon/>}
+            icon={<CalendarMonthIcon />}
             component={<Link to="/home/bookings" />}
           >
             Bookings
           </MenuItem>
-          
-            {/* <MenuItem
-            onClick={handleShow}
-            icon={<i className="fa-solid fa-unlock"></i>}
-          >
-            Change Password
-          </MenuItem> */}
 
           <MenuItem
             onClick={logOut}
-            icon={<LogoutIcon/>}
+            icon={<LogoutIcon />}
           >
             Logout
           </MenuItem>
-         
         </Menu>
       </Sidebar>
     </div>
   );
 };
-export default SideBar;
 
+export default SideBar;
