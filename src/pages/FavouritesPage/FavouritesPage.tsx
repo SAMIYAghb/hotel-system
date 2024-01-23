5031ca25a79fa8a7c5ae936987bbfa30781c0ca9
Hadeer Magdy <dody.magdy711@gmail.com>
2024-01-23 14:30:44 +0200

    add remove fav functionality

--- src/pages/FavouritesPage/FavouritesPage.tsx
+++ src/pages/FavouritesPage/FavouritesPage.tsx
@@ -1,12 +1,7 @@
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
-import {
-  addToFavUrl,
-  allFavRoomsUrl,
-  allRoomsFilterdUrl,
-  removeFavRoomUrl,
-} from "../../services/api";
-import { Container, Box } from "@mui/system";
+import { allFavRoomsUrl, removeFavRoomUrl } from "../../services/api";
+import {  Box } from "@mui/system";
 import NavBar from "../../features/User/Ui/NavBar/NavBar";
 import Footer from "../../features/User/Ui/Footer/Footer";
 import { CardContent, Grid, IconButton, Typography } from "@mui/material";
@@ -17,10 +12,12 @@ import Style from "./Fav.module.css";
 import Loader from "../../shared/Loader/Loader";
 import { AuthContext } from "../../context/AuthContext";
 import { toast } from "react-toastify";
+import { useParams } from "react-router";
 
 const FavouritesPage: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const { requestHeaders } = useContext(AuthContext);
+  // const { roomId } = useParams();
 
   // **********************************
   const [favRoomsList, setFavRoomsList] = useState([]);
@@ -28,11 +25,10 @@ const FavouritesPage: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalCount, setTotalCount] = useState(0);
   const totalPages = Math.ceil(totalCount / 6);
-  // ****************************
-  const [favStatus, setFavStatus] = useState({}); // To update the status of Fav
 
   // ***********getAllFavRooms*****************
   const getAllFavRooms = () => {
+    setIsLoading(true);
     axios
       .get(`${allFavRoomsUrl}`, {
         headers: requestHeaders,
@@ -42,7 +38,6 @@ const FavouritesPage: React.FC = () => {
           "succ fet fav",
           response?.data?.data?.favoriteRooms[0].rooms
         );
-        setIsLoading(true);
         setFavRoomsList(response?.data?.data?.favoriteRooms[0].rooms);
         // setTotalCount(response.data.data.totalCount);
         // setCurrentPage(page);
@@ -56,17 +51,14 @@ const FavouritesPage: React.FC = () => {
   };
   // ***********removeFavRooms*****************
   const removeFromFav = (roomId: string) => {
+    // console.log("room id",room?._id)
     axios
-      .delete(
-        `${removeFavRoomUrl}/${roomId}`,
-        {
-          headers: requestHeaders,
-        }
-      
-      )
+      .delete(`${removeFavRoomUrl}/${roomId}`, {
+        headers: requestHeaders,
+      })
 
       .then((response) => {
-        console.log("succ remove", response.data.data.rooms);
+        console.log("succ remove", response);
         toast.success("Removed from Favorite Successfully");
         // setIsLoading(true);
         // setFavRoomsList(response.data.data.rooms);
@@ -80,19 +72,6 @@ const FavouritesPage: React.FC = () => {
         setIsLoading(false);
       });
   };
-
-  // Load favStatus from localStorage on component mount
-  useEffect(() => {
-    const storedFavStatus = localStorage.getItem("favStatus");
-    if (storedFavStatus) {
-      setFavStatus(JSON.parse(storedFavStatus));
-    }
-  }, []);
-
-  // Save favStatus to localStorage whenever it changes
-  useEffect(() => {
-    localStorage.setItem("favStatus", JSON.stringify(favStatus));
-  }, [favStatus]);
 
   useEffect(() => {
     getAllFavRooms();
@@ -113,7 +92,6 @@ const FavouritesPage: React.FC = () => {
       <Grid container spacing={2}>
         {isLoading ? (
           <div className="centered">
-            {" "}
             <Loader />
           </div>
         ) : (
@@ -124,9 +102,6 @@ const FavouritesPage: React.FC = () => {
                   <CardContent>
                     {room?.images && room?.images.length > 0 ? (
                       <div className={Style.imageWrapper}>
-                        {/* <span className={Style.badge}>
-                    ${room?.price} per night
-                  </span> */}
                         <img
                           src={
                             room?.images.length > 1
@@ -145,12 +120,12 @@ const FavouritesPage: React.FC = () => {
                             justifyContent="center"
                             alignItems="center"
                           >
-                            <IconButton onClick={() => removeFromFav(room._id)}>
+                            <IconButton
+                              onClick={() => removeFromFav(room?._id)}
+                            >
                               <FavoriteIcon
                                 style={{
-                                  color: favStatus[room._id]
-                                    ? "#f50057"
-                                    : "white",
+                                  color: "#f50057",
                                 }}
                               />
                             </IconButton>
@@ -161,9 +136,6 @@ const FavouritesPage: React.FC = () => {
                       </div>
                     ) : (
                       <div className={Style.imageWrapper}>
-                        {/* <span className={Style.badge}>
-                    ${room?.price} per night
-                  </span> */}
                         <img
                           className="imgAnimate"
                           src={noHotelImg}
@@ -179,10 +151,13 @@ const FavouritesPage: React.FC = () => {
                             justifyContent="center"
                             alignItems="center"
                           >
-                            <IconButton onClick={() => removeFromFav(room._id)}>
+                            <IconButton
+                              onClick={() => removeFromFav(room?._id)}
+                            >
                               <FavoriteIcon
-                          
-                              style={{ color: favStatus[room._id] ? '#f50057' : 'white' }}
+                                style={{
+                                  color: "#f50057",
+                                }}
                               />
                             </IconButton>
 
